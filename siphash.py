06a6d8b4973a8300da08fe0ecd5367859fbbc0f5
Jinxuan <i@cjxol.com>
2022-07-08 17:52:04 +0100

    SipHash

---
+++ siphash.py
@@ -0,0 +1,209 @@
+from typing import List, Tuple
+import binascii
+from util import big_to_little8, rotl8
+
+
+class SipHash:
+
+    # TODO: Change ways to pass parameters in a more object-oriented way.
+
+    """
+    Implemented following the algorithm in (Aumasson and Bernstein, 2012).
+
+    Aumasson, JP., Bernstein, D.J. (2012). SipHash: A Fast Short-Input PRF.
+    In: Galbraith, S., Nandi, M. (eds) Progress in Cryptology - INDOCRYPT 2012.
+    INDOCRYPT 2012. Lecture Notes in Computer Science, vol 7668.
+    Springer, Berlin, Heidelberg. https://doi.org/10.1007/978-3-642-34931-7_28
+    """
+
+    def __init__(self, key: int, message: bytes, c=2, d=4) -> None:
+        """
+        Initialise SipHash with a key and message.
+
+        Parameters
+        ----------
+        key : int
+            16-byte big-endian key
+        message : bytes
+            Message to be hashed in big-endian bytes
+        c : int
+            Number of compression rounds
+        d : int
+            Number of finalization round
+        """
+        self.key = key
+        self.message = message
+        self.c = c
+        self.d = d
+        self.hash = None
+
+    def get_hash(self) -> int:
+        """
+        Return hash of the message hashed with the key.
+
+        Return from saved value if the hash has been calculated, or calculate the hash value and save it and return.
+
+        Returns
+        -------
+        int
+            Hash value in little-endian
+        """
+        if self.hash is not None:
+            return self.hash
+        
+        k0, k1 = self._encode_key(self.key)
+        internal_state = self._initialise_internal_state(k0, k1)
+        internal_state = self._compress(self.message, internal_state)
+        return self._finalise(internal_state)
+
+    def hexdigest(self) -> str:
+        """
+        Return the hex string of the hash.
+
+        Returns
+        -------
+        str
+            Hex string of the hash
+        """
+        return hex(self.get_hash())[2:]
+
+    def _encode_key(self, key: int) -> Tuple[int, int]:
+        """
+        Encode 16-byte key into 8-byte k0 and k1.
+
+        Parameters
+        ----------
+        key : int
+            16-byte big-endian key
+
+        Returns
+        -------
+        (bytes, bytes)
+            Tuple of k0 and k1
+        """
+        return (big_to_little8(key >> 8 * 8), big_to_little8(key & 0xffffffffffffffff))
+
+    def _initialise_internal_state(self, k0: int, k1: int) -> Tuple[int, int, int, int]:
+        """
+        Initialise internal state v0, v1, v2, v3.
+
+        Parameters
+        ----------
+        k0 : int
+            8-byte k0
+        k1 : int
+            8-byte k1
+
+        Returns
+        -------
+        (int, int, int, int)
+            Internal state v0, v1, v2, v3
+        """
+        c1 = 0x736f6d6570736575
+        c2 = 0x646f72616e646f6d
+        c3 = 0x6c7967656e657261
+        c4 = 0x7465646279746573
+        v0 = k0 ^ int(c1)
+        v1 = k1 ^ int(c2)
+        v2 = k0 ^ int(c3)
+        v3 = k1 ^ int(c4)
+        return (v0, v1, v2, v3)
+
+    def _compress(self, message: bytes, internal_state: Tuple[int, int, int, int]) -> Tuple[int, int, int, int]:
+        """
+        Compress the message into internal state.
+
+        Parameters
+        ----------
+        message : bytes
+            Message to be hashed in big endian bytes
+        internal_state : (int, int, int, int)
+            Internal state v0, v1, v2, v3
+
+        Returns
+        -------
+        (int, int, int, int)
+            Internal state with message compressed into
+        """
+        words = self._message_to_words(message)
+
+        v0, v1, v2, v3 = internal_state
+        for word in words:
+            v3 ^= word
+            for _ in range(self.c):
+                v0, v1, v2, v3 = self._sipround((v0, v1, v2, v3))
+            v0 ^= word
+        
+        return (v0, v1, v2, v3)
+
+    def _message_to_words(self, message: bytes) -> List[int]:
+        """
+        Parse message into words
+
+        Parameters
+        ----------
+        message : bytes
+            Message to be hashed in big endian bytes
+
+        Returns
+        -------
+        [int]
+            Message parsed into little-endian words
+        """
+        message_length = len(message)
+        padding_length = (message_length + 1) % 8
+        message = message + b'\x00' * padding_length + (message_length % 256).to_bytes(1, 'little')
+        return [int.from_bytes(message[i: i + 8], 'little') for i in range(0, len(message), 8)]
+
+    def _sipround(self, internal_state: Tuple[int, int, int, int]) -> Tuple[int, int, int, int]:
+        """
+        SipRound to transform internal state.
+
+        Parameters
+        ----------
+        internal_state : (int, int, int, int)
+            Internal state v0, v1, v2, v3
+
+        Returns
+        -------
+        (int, int, int, int)
+            Internal state v0, v1, v2, v3 after SipRound transform
+        """
+        v0, v1, v2, v3 = internal_state
+
+        v0 = (v0 + v1) & 0xffffffffffffffff
+        v1 = rotl8(v1, 13)
+        v1 ^= v0
+        v0 = rotl8(v0, 32)
+        v2 = (v2 + v3) & 0xffffffffffffffff
+        v3 = rotl8(v3, 16)
+        v3 ^= v2
+        v2 = (v2 + v1) & 0xffffffffffffffff
+        v1 = rotl8(v1, 17)
+        v1 ^= v2
+        v2 = rotl8(v2, 32)
+        v0 = (v0 + v3) & 0xffffffffffffffff
+        v3 = rotl8(v3, 21)
+        v3 ^= v0
+
+        return (v0, v1, v2, v3)
+
+    def _finalise(self, internal_state: Tuple[int, int, int, int]) -> int:
+        """
+        Finalise SipHash
+
+        Parameters
+        ----------
+        internal_state : (int, int, int, int)
+            Internal state before finalise
+
+        Returns
+        -------
+        int
+            SipHash result in little-endian representation
+        """
+        v0, v1, v2, v3 = internal_state
+        v2 ^= 0xff
+        for _ in range(self.d):
+            v0, v1, v2, v3 = self._sipround((v0, v1, v2, v3))
+        return v0 ^ v1 ^ v2 ^ v3
---
+++ test_siphash.py
@@ -0,0 +1,70 @@
+import unittest
+import binascii
+from siphash import SipHash
+
+
+class TestSipHash(unittest.TestCase):
+
+    def setUp(self):
+        key = 0x000102030405060708090a0b0c0d0e0f
+        message = binascii.unhexlify(b'000102030405060708090a0b0c0d0e')
+        self.siphash = SipHash(key, message)
+
+    def test_encode_key(self):
+        key = 0x000102030405060708090a0b0c0d0e0f
+        k0 = 0x0706050403020100
+        k1 = 0x0f0e0d0c0b0a0908
+        self.assertEqual(self.siphash._encode_key(key), (k0, k1))
+
+    def test_initialise_internal_state(self):
+        v0 = 0x7469686173716475
+        v1 = 0x6b617f6d656e6665
+        v2 = 0x6b7f62616d677361
+        v3 = 0x7b6b696e727e6c7b
+        self.assertEqual(self.siphash._initialise_internal_state(0x0706050403020100, 0x0f0e0d0c0b0a0908), (v0, v1, v2, v3))
+
+    def test_compress(self):
+        v0 = 0x7469686173716475
+        v1 = 0x6b617f6d656e6665
+        v2 = 0x6b7f62616d677361
+        v3 = 0x7b6b696e727e6c7b
+        v0_compression = 0x3c85b3ab6f55be51
+        v1_compression = 0x414fc3fb98efe374
+        v2_compression = 0xccf13ea527b9f4bd
+        v3_compression = 0x5293f5da84008f82
+        message = binascii.unhexlify(b'000102030405060708090a0b0c0d0e')
+        self.assertEqual(self.siphash._compress(message, (v0, v1, v2, v3)), (v0_compression, v1_compression, v2_compression, v3_compression))
+
+    def test_message_to_words(self):
+        message = binascii.unhexlify(b'000102030405060708090a0b0c0d0e')
+        self.assertListEqual(self.siphash._message_to_words(message), [0x0706050403020100, 0x0f0e0d0c0b0a0908])
+
+    def test_sipround(self):
+        v0 = 0x7469686173716475
+        v1 = 0x6b617f6d656e6665
+        v2 = 0x6b7f62616d677361
+        v3 = 0x7c6d6c6a717c6d7b
+        v0_sipround2 = 0x4d07749cdd0858e0
+        v1_sipround2 = 0x0d52f6f62a4f59a4
+        v2_sipround2 = 0x634cb3577b01fd3d
+        v3_sipround2 = 0xa5224d6f55c7d9c8
+        sipround1 = self.siphash._sipround((v0, v1, v2, v3))
+        sipround2 = self.siphash._sipround(sipround1)
+        self.assertEqual(sipround2, (v0_sipround2, v1_sipround2, v2_sipround2, v3_sipround2))
+
+    def test_finalise(self):
+        v0 = 0x3c85b3ab6f55be51
+        v1 = 0x414fc3fb98efe374
+        v2 = 0xccf13ea527b9f4bd
+        v3 = 0x5293f5da84008f82
+        h = 0xa129ca6149be45e5
+        self.assertEqual(self.siphash._finalise((v0, v1, v2, v3)), h)
+
+    def test_get_hash(self):
+        self.assertEqual(self.siphash.get_hash(), 0xa129ca6149be45e5)
+
+    def test_hexdigest(self):
+        self.assertEqual(self.siphash.hexdigest(), 'a129ca6149be45e5')
+
+if __name__ == '__main__':
+    unittest.main()
